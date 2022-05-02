994e055f55215bea3bb3675f602a7916e804faeb
sk0g <ani.mehta@mail.com>
2022-05-02 20:01:48 +1000

    Tidy and update serial processor

--- serial.py
+++ serial.py
@@ -9,39 +9,32 @@ uart_parity_bits = None
 uart_stop_bits = 1
 uart_timeout = 2000
 
-value_update = re.compile("(\d{1,2})\|(\d\.?\d{0,2})")
-set_port_direction = re.compile("P\|(\d{1,2})")
+value_update = re.compile(r'(\d{2})\|(\d\.?\d{0,2})')
+set_port_input = re.compile(r'PI\|(\d{2})')
+set_port_output = re.compile(r'PO\|(\d{1,2})')
 
 
-def process_port_set(matches):
-    print(f"Processing port set with {matches=}")
+def process_port_set(matches, is_input: bool):
+    print(f"Processing port set with {matches=}, {is_input=}")
 
 
-def send_value(matches):
+def set_value(matches):
     print(f"Sending value to port with {matches=}")
 
 
-def receive_value(line: str):
-    split = line.split("|")
-
-    if len(split) <= 1:
-        print(f"Splitting receive value command with malformed {line=}")
-
-    port_num = int(split[0])
-    value = float(split[1])
-
-    print(f"Received {value=} from {port_num=}")
-
-
 def process_line(line: str):
+    line = line.strip()
     print(f"Processing {line=}")
 
     if line == "reset":
         print("resetting")
         machine.reset()
 
-    elif (m := value_update.match(line)):
-        send_value(m)
+    elif m := value_update.match(line):
+        set_value(m)
 
-    elif (m := set_port_direction.match(line)):
-        process_port_set(m)
+    elif m := set_port_input.match(line):
+        process_port_set(m, is_input=True)
+
+    elif m := set_port_output.match(line):
+        process_port_set(m, is_input=False)
