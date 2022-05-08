5d87b9b444e9e140c091592186e711a96d85ddaf
sk0g <ani.mehta@mail.com>
2022-05-08 21:09:52 +1000

    Add start/ cleanup code

--- second_core.py
+++ second_core.py
@@ -1,12 +1,14 @@
 import utime
 
+from device import DeviceController
+
 SLEEP_BETWEEN_LOOPS_US = 50
 
 current_time_us: int = 0
-tasks: list["CoreTask"] = []
+tasks: list["PulseTask"] = []
 
 
-class CoreTask:
+class PulseTask:
     till_time_us: int
     port_number: int
     started: bool = False
@@ -14,17 +16,18 @@ class CoreTask:
     def __init__(self, port_number, duration_us):
         self.port_number = port_number
         self.till_time_us = current_time_us + duration_us
+        DeviceController.pin(port_number).set_value(1)
 
     def is_done(self) -> bool:
         return current_time_us >= self.till_time_us
 
     def cleanup(self):
         # turn port off
-        pass
+        DeviceController.pin(self.port_number).set_value(0)
 
 
 def add_task(port_num: int, duration_us: int):
-    tasks.append(CoreTask(port_num, duration_us))
+    tasks.append(PulseTask(port_num, duration_us))
 
 
 def core_task():
@@ -37,8 +40,8 @@ def core_task():
         # Loop over current tasks, looking for ones that are completed
         for index, task in enumerate(tasks):
             if task.is_done():
-                completed_task_indices.append(index)
                 task.cleanup()
+                completed_task_indices.append(index)
 
         # Remove any completed tasks
         for index in completed_task_indices:
